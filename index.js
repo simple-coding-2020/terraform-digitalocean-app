78fd9fe33f61b281c57eb4db93fa11c31fa0b66a
Simple Coding <simple.coding.2020@gmail.com>
2020-04-26 16:55:40 +0200

    Port is not default

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/time', (req, res) => {
         user     : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
         database : process.env.DB_DATABASE,
+        port     : process.env.DB_PORT,
     })
 
     connection.connect()
