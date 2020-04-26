f9dd0638dd3ad22e50a4c39a56b9e12d1441831b
Simple Coding <simple.coding.2020@gmail.com>
2020-04-26 10:49:21 +0200

    Add MySQL connection

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express')
+const os = require('os')
+const app = express()
+const mysql = require('mysql')
+
+app.get('/', (req, res) => res.send(`Hello world from ${os.hostname()}!`))
+
+app.get('/time', (req, res) => {
+    const connection = mysql.createConnection({
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_DATABASE,
+    })
+
+    connection.connect()
+
+    connection.query('SELECT NOW() AS cTime', (error, results) => {
+        if (error) throw error
+        res.send(`Current time is: ${results[0].cTime}`)
+    })
+
+    connection.end()
+})
 
 app.listen(8080)
 
